1efe6f14f9898a8df42e9268017a3d21f9d3a5ac
hunaric <nathanaelfangnon@gmail.com>
2025-01-05 19:21:55 +0100

    Landlord page is done and reservations start

--- django-housing/property/api.py
+++ django-housing/property/api.py
@@ -16,6 +16,14 @@ from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, R
 @permission_classes([])
 def properties_list(request):
     properties = Property.objects.all().order_by('-created_at')
+
+    # Filter
+    landlord_id = request.GET.get('landlord_id', '')
+
+    if landlord_id:
+        properties = properties.filter(landlord_id=landlord_id)
+    
+    # Not filtered
     serializer = PropertiesListSerializer(properties, many=True)
 
     return JsonResponse({
