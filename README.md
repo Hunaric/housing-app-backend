4fa1c3f796a5ed09d945eff42f192a1f3a808d0b
hunaric <nathanaelfangnon@gmail.com>
2025-02-12 20:20:39 +0100

    Start chat app

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 To authorize any user to edit file inside the project: `sudo chown -R $USER:$USER ~/Documents/Projects/Python`
 Command to make superuser with docker compose: `docker compose exec web python manage.py createsuperuser`
-Command to run docker server: `$docker compose up --build` and to do the same in the background, change '--build' by '-d'
+Command to run docker server: `docker compose up --build` and to do the same in the background, change '--build' by '-d'
+
+To vizualize your database, apply this: `docker compose exec db psql -U postgresuser -d djangobnb`
+Then enter (For example): '\d chat_conversationmessage;'
--- django-housing/chat/models.py
+++ django-housing/chat/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from useraccount.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    users = models.ManyToManyField(User, related_name='conversations')  # Changed to plural for clarity
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)  # Changed to auto_now=True
+
+    def __str__(self):
+        return f"Conversation {self.id} with users {', '.join(str(user) for user in self.users.all())}"
+
+class ConversationMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
+    body = models.TextField()
+    sent_to = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.id} in conversation {self.conversation.id} by {self.created_by}"
