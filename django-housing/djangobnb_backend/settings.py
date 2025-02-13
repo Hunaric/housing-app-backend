501ddfc6f2bd86b5846fb7ab5f43dd95fdd0e9e8
hunaric <nathanaelfangnon@gmail.com>
2025-02-13 01:54:48 +0100

    Sockets installed and configured. Basic consumers are made and routing is set

--- django-housing/djangobnb_backend/settings.py
+++ django-housing/djangobnb_backend/settings.py
@@ -24,6 +24,12 @@ SITE_ID = 1
 
 WEBSITE_URL = 'http://localhost:8000'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayers'
+    }
+}
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
@@ -70,6 +76,7 @@ REST_AUTH = {
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,6 +133,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangobnb_backend.wsgi.application'
+ASGI_APPLICATION = 'djangobnb_backend.asgi.application'
 
 
 # Database
