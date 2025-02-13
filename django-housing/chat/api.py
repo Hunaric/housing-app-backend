03f3a446dc4ec0335b11f9f0c0e8b2dae20fb096
hunaric <nathanaelfangnon@gmail.com>
2025-02-13 15:06:31 +0100

    i edit the api to add one conversation by the id and create his url to fetch in the front end

--- django-housing/chat/api.py
+++ django-housing/chat/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from .models import Conversation
-from .serializers import ConversationListSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSerializer
 
 
 @api_view(['GET'])
@@ -12,4 +12,15 @@ def conversation_list(request):
 
     return JsonResponse({
         'data': serializer.data
+    }, safe=False)
+
+
+@api_view(['GET'])
+def conversation_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+
+    conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+
+    return JsonResponse({
+        'conversation': conversation_serializer.data
     }, safe=False)
--- django-housing/chat/urls.py
+++ django-housing/chat/urls.py
@@ -5,4 +5,5 @@ from . import api
 
 urlpatterns = [
     path('', api.conversation_list, name='api_conversations_list'),
+    path('<uuid:pk>/', api.conversation_detail, name='api_conversations_detail'),
 ]
