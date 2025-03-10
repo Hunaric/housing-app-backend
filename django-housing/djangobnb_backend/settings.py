82b2449a2dc4f9ccf5e933e489b03d17ca93b49a
hunaric <nathanaelfangnon@gmail.com>
2025-03-11 00:22:19 +0100

    adjust 2

--- django-housing/djangobnb_backend/settings.py
+++ django-housing/djangobnb_backend/settings.py
@@ -64,6 +64,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:4200',
     'http://localhost:4200',
+    'https://housing-app-349h.vercel.app/',
 ]
 
 # CORS_ALLOWED_ALL_ORIGINS = True
